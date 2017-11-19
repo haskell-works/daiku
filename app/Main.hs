98fd3ee54ba506808668c28c854e1d70763c3bb9
John Ky <newhoggy@gmail.com>
2017-11-19 18:15:53 +1100

    Initial project

---
+++ app/Main.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import Development.Shake
+import Development.Shake.Command
+import Development.Shake.FilePath
+import Development.Shake.Util
+import System.Directory
+
+main :: IO ()
+main = do
+  createDirectoryIfMissing True "_shake"
+  runShake
+
+runShake :: IO ()
+runShake = shakeArgs shakeOptions{shakeFiles="_build"} $ do
+  want ["_build/run" <.> exe]
+
+  phony "clean" $ do
+    putNormal "Cleaning files in _build"
+    removeFilesAfter "_build" ["//*"]
+
+  "_build/run" <.> exe %> \out -> do
+    cs <- getDirectoryFiles "" ["//*.c"]
+    let os = ["_build" </> c -<.> "o" | c <- cs]
+    need os
+    cmd "gcc -o" [out] os
+
+  "_build//*.o" %> \out -> do
+    let c = dropDirectory1 $ out -<.> "c"
+    let m = out -<.> "m"
+    () <- cmd "gcc -c" [c] "-o" [out] "-MMD -MF" [m]
+    needMakefileDependencies m
