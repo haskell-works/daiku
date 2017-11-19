7bc720a9af5f5f5cf9941e9fbe0e715dbac104da
John Ky <newhoggy@gmail.com>
2017-11-19 21:23:47 +1100

    New createHelloBucket function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( createHelloBucket
+  ) where
+
+import Control.Lens
+import Control.Monad.Trans.AWS
+import Network.AWS.S3
+import System.IO
+
+createHelloBucket :: IO ()
+createHelloBucket = do
+  lgr <- newLogger Trace stdout
+  env <- newEnv Discover <&> set envLogger lgr . set envRegion Oregon
+  _ <- runResourceT . runAWST env $ send $ createBucket "daiku-hello" &
+    (cbCreateBucketConfiguration .~ Just (createBucketConfiguration & cbcLocationConstraint .~ Just (LocationConstraint Oregon)))
+  return ()
