c01e5ee909fa05f1ef5deef11fc861d4da18e97b
John Ky <newhoggy@gmail.com>
2017-12-14 16:09:34 -0800

    Experiment with YAML

--- src/Daiku/Pod.hs
+++ src/Daiku/Pod.hs
@@ -1,8 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
 module Daiku.Pod where
 
+import Data.Yaml         (FromJSON (..), (.:))
+import Text.RawString.QQ
+
 import qualified Data.Yaml as Y
 
+data Pod = Pod
+  { inputs  :: [String]
+  , outputs :: [String]
+  } deriving (Eq, Show)
+
+instance FromJSON Pod where
+  parseJSON (Y.Object v) = Pod
+    <$> v .:  "inputs"
+    <*> v .:  "outputs"
+  parseJSON _ = fail "Expected Object for Pod value"
+
 main :: IO ()
-main = print (Y.decode "[1, 2, 3]" :: Maybe [Integer])
+main = print (Y.decode [r|
+inputs:
+- input1
+- input2
+outputs:
+- output1
+- output2
+|] :: Maybe Pod)
